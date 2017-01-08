8fa2a7e04e553f423e88581f6403b64031704ff2
ElWilbo <william.knighting@yahoo.com>
2017-01-08 00:48:43 -0800

    Add files via upload

--- kqt_app.py
+++ kqt_app.py
@@ -2,11 +2,6 @@
 import tkinter as tk
 import sqlite3 as lite 
 
-#CONNECT TO DB
-con = lite.connect('snohomish.db')
-cur = con.cursor()
-data_pull = cur.execute('SELECT * FROM MAIN LIMIT 25')
-
 
 class kqt_app(tk.Frame):
 	def __init__(self,parent):
@@ -14,38 +9,61 @@ class kqt_app(tk.Frame):
 		self.parent = parent
 		self.initialize()
 		self.db_connect()
+		self.data_pull=[]
 		
 	#Connect to the Snohomish DB and pull 25 records to fill first instance of the Results table
 	def db_connect(self):
-		con = lite.connect('adj_snohomish.db')
-		cur = con.cursor()
-		data_pull = cur.execute('SELECT * FROM MAIN LIMIT 25')
+		self.con = lite.connect('adj_snohomish.db')
+		self.cur = self.con.cursor()
+		self.data_pull = self.cur.execute('SELECT * FROM MAIN LIMIT 25')
 	
 	#Function to Delete items in the List Box
 	def listbox_del(self, event=None):
 		self.results_listbox.delete(tk.ANCHOR)
 	
-		
+	#Function for putting search results into the listbox
+	def insert_data(self):
+		self.results_listbox.delete(0,tk.END)
+		for data in self.data_pull:
+			self.results_listbox.insert(tk.END, data)
+			
 	#GENERATE SEARCH FUNCTION:
 	def generate_search(self):
 		self.city_var = str(self.entry_city.get())
-		print(type(self.entry_type.get()))
-		print(self.entry_zip.get())
-		print(self.entry_value.get())
+		self.type_var = str(self.entry_type.get())
+		self.zip_var = str(self.entry_zip.get())
+		self.value_var = str(self.entry_value.get())
+		self.name_var = str(self.entry_name.get())
 		
 		#USING THE ALL PLACE HOLDER WILL DO A SELECT ALL QUERY
-		if self.entry_city.get() == self.entry_type.get() == self.entry_zip.get() == self.entry_value.get() == 'ALL':
-			data_pull = cur.execute('SELECT * FROM MAIN LIMIT 25')
-			self.results_listbox.delete(0,tk.END)
-			for data in data_pull:
-				self.results_listbox.insert(tk.END, data)
+		if self.city_var == self.type_var == self.zip_var == self.value_var == self.name_var == 'ALL':
+			self.data_pull = self.cur.execute('SELECT * FROM MAIN LIMIT 25')
+			self.insert_data()
+	
+		#IF ONLY CITY IS ENTERED  
+		if self.city_var != 'ALL':
+			self.data_pull = self.cur.execute('SELECT * FROM MAIN WHERE SitusCity = (?) LIMIT 25', (self.city_var,))
+			self.insert_data()
 		
-		#IF ONLY CITY IS ENTERED  ?!?!?!?!?!?!?!?!?!?!?!?!?!?
-		if self.entry_city != 'ALL':
-			data_pull = cur.execute('SELECT * FROM MAIN WHERE SitusCity = ? LIMIT 25', (self.city_var,))
-			self.results_listbox.delete(0,tk.END)
-			for data in data_pull:
-				self.results_listbox.insert(tk.END, data)
+		#IF ONLY TYPE IS ENTERED
+		if self.type_var != 'ALL':
+			self.data_pull = self.cur.execute('SELECT * FROM MAIN WHERE USE = (?) LIMIT 25', (self.type_var,))
+			self.insert_data()
+		
+		#IF ONLY ZIP IS ENTERED
+		if self.zip_var != 'ALL':
+			self.data_pull = self.cur.execute('SELECT * FROM MAIN WHERE ZipCode = (?) LIMIT 25', (self.zip_var,))
+			self.insert_data()
+		
+		#IF ONLY VALUE IS ENTERED
+		if self.value_var != 'ALL':
+			self.data_pull = self.cur.execute('SELECT * FROM MAIN WHERE MKTTL < (?) LIMIT 25', (self.value_var,))
+			self.insert_data()
+		
+		#IF ONLY CITY,VALUE IS ENTERED
+		 #?????
+		
+		
 		
 	#********CREATES THE GUI********************	
 	def initialize(self):
@@ -63,7 +81,7 @@ class kqt_app(tk.Frame):
 		#INSERT WIDGETS
 		self.label_city = tk.Label(self.city_area, text = 'City', background='pink')
 		self.label_city.grid(row=1,column=1)
-		
+		#
 		self.entry_city = tk.Spinbox(self.city_area, values=('ALL','EVERETT','MARYSVILLE','LYNNWOOD','BOTHELL','SNOHOMISH','EDMONDS','ARLINGTON','LAKE STEVENS','STANDWOOD','MONROE','MUKILTEO','MOUNTLAKE TERRACE', 'MILL CREEK', 'GRANITE FALLS', 'SULTAN', 'GOLD BAR', 'BRIER', 'SNOHOMISH COUNTY', 'WOODINVILLE', 'TULALIP', 'DARRINGTON'))
 		self.entry_city.grid(row=2,column=1)
 		
@@ -73,7 +91,7 @@ class kqt_app(tk.Frame):
 		##INSERT WIDGETS 
 		self.label_type = tk.Label(self.property_area, text ='Property Type', background='pink')
 		self.label_type.grid(row=1,column=1)
-		
+		#
 		self.entry_type = tk.Spinbox(self.property_area, width = 45, values=('ALL','111 Single Family Residence - Detached', '910 Undeveloped (Vacant) Land', '143 Single Family Residence Condominium Multiple','118 Manufactured Home (Owned Site)',' Single Family Residence Condominium Detached', '119 Manufactured Home (Mobile Home Park)','119 Vacant Site Mobile Home Park', '122 Two Family Residence (Duplex)', '145 Condominium Conversion','142 Single Family Residence Condominium Common Wall', '144 Single Family Residence Conominium Project', 'Four Family Residence (Flour Plex)', '198 Vacation Cabin'))
 		self.entry_type.grid(row=2,column=1)
 			
@@ -83,7 +101,7 @@ class kqt_app(tk.Frame):
 		#INSERT WIDGETS
 		self.label_zip = tk.Label(self.zip_area, text ='Zip Code', background = 'pink')
 		self.label_zip.grid(row=1,column=1)
-		
+		#
 		self.entry_zip = tk.Spinbox(self.zip_area, values=('ALL','98012','98223','98208','98270','98258','98290'))
 		self.entry_zip.grid(row=2,column=1)
 		
@@ -91,17 +109,36 @@ class kqt_app(tk.Frame):
 		self.value_area = tk.Frame(self.search_frame, background='pink',padx=5,pady=5)
 		self.value_area.grid(row=1,column=4)
 		#INSERT WIDGETS
-		self.label_value = tk.Label(self.value_area, text ='Assessor Value', background='pink')
+		self.label_value = tk.Label(self.value_area, text ='Assessor Value (MAXIMUM)', background='pink')
 		self.label_value.grid(row=1,column=1)
 		#
-		self.entry_value = tk.Spinbox(self.value_area, values=('ALL'),)
+		self.entry_value = tk.Spinbox(self.value_area, values=('ALL'))
 		self.entry_value.grid(row=2,column=1)
+		
+		#LastName Value FRAME
+		self.name_area = tk.Frame(self.search_frame, background = 'pink',padx=5,pady=5)
+		self.name_area.grid(row=1,column=5)
+		#INSERT WIDGETS
+		self.label_name = tk.Label(self.name_area, text = 'Last Name', background='pink')
+		self.label_name.grid(row=1,column=1)
+		#
+		self.entry_name = tk.Spinbox(self.name_area, values=('ALL'))
+		self.entry_name.grid(row=2,column=1)
+		
+		#StreetName Value FRAME
+		self.street_area = tk.Frame(self.search_frame, background = 'pink', padx=5,pady=5)
+		self.street_area.grid(row=1,column=6)
+		#INSERT WIDGETS
+		self.label_street = tk.Label(self.street_area, text = 'Street Name', background='pink')
+		self.label_street.grid(row=1,column=1)
+		#
+		self.entry_street = tk.Spinbox(self.street_area, values=('ALL'))
+		self.entry_street.grid(row=2,column=1)
+		
 		
 		#SEARCH BUTTON
 		self.search_button = tk.Button(self.search_frame, text='Search', command=self.generate_search, background='white')
-		self.search_button.grid(row=2,column=4)
-		
-		
+		self.search_button.grid(row=2,column=6)
 		
 		######RESULTS PANEL#########
 		self.results_panel = tk.LabelFrame(self, text = 'Results', padx=3,pady=3, background = 'grey')
@@ -116,9 +153,6 @@ class kqt_app(tk.Frame):
 		self.generate_letter_button = tk.Button(self.results_panel, text = 'Generate Letters', command= lambda: print(self.results_listbox.get(0,tk.END)))
 		self.generate_letter_button.pack(side=tk.BOTTOM)
 		
-		#Initial Results Panel Data
-		for data in data_pull:
-			self.results_listbox.insert(tk.END, data)
 
 if __name__ =='__main__':
 	app=kqt_app(None)
